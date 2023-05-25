7c4d680bcda4c0a6b544faa8de4939de9edcd651
Rodrigo <rodrigoml2395@gmail.com>
2023-05-25 18:06:12 -0300

    feat: hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Services from './components/Services';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
 
 const App = () => {
   return (
     <main>
+      <Hero />
       <About />
+      <Services />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+      <Footer /> 
     </main>
   )
 }
