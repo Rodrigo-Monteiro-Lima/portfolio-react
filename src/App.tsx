78ae36442673dbb0abaa4dfdec902e3032e287c1
Rodrigo <rodrigoml2395@gmail.com>
2023-06-19 17:14:00 -0300

    fix: imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css';
 import About from './components/About';
-import Carousel from './components/Carrousel';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Services from './components/Services';
 import Skills from './components/Skills';
-import Testimonials from './components/Testimonials';
 
 const App = () => {
   return (
@@ -17,7 +15,6 @@ const App = () => {
       <Services />
       <Projects />
       <Skills />
-      {/* <Testimonials /> */}
       <Contact />
       <Footer /> 
     </main>
